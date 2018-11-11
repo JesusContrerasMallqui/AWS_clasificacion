8e4446da91771aa62c0d07f9437a5a2bd51c2e98
JesusContrerasMallqui <44751559+JesusContrerasMallqui@users.noreply.github.com>
2018-11-11 00:52:02 -0500

    Update service.py

--- Clasificacion/scripts/service.py
+++ Clasificacion/scripts/service.py
@@ -21,21 +21,22 @@ def rayosx():
 @app.route('/Clasificacion/default/', methods=['GET','POST'])
 def default():
 	print (request.args)
-	# dimensions of our images.
-	img_width, img_height = 50, 50
 	# Show
 	image_name = request.args.get("imagen")
-	img_path='../samples/'+image_name
-	img = image.load_img(img_path, target_size=(img_width, img_height))
-	img = image.img_to_array(img)
-	
-	with graph.as_default():
-		result = loaded_model.predict(img)
-		if result[0][0] == 1:
-    			print(result[0][0], ' --> Es un perro')
-		else:
-		    	print(result[0][0], ' --> Es un gato ')
-		return result
+	test_image_path = './dataset/samples/3.jpg'
+test_image = image.load_img(test_image_path)
+plt.imshow(test_image)
+plt.show()
+
+test_image = image.load_img(test_image_path,target_size = (50, 50))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = loaded_model.predict(test_image)
+if result[0][0] == 1:
+    print(result[0][0], ' --> Es un perro')
+else:
+    print(result[0][0], ' --> Es un gato ')
+
 
 # Run de application
 app.run(host='0.0.0.0',port=5000)
